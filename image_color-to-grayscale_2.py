1fd8570996e4e5305cfe359ac7baa05d83d23681
Rajasekaran A <rajasekaran.al@gmail.com>
2022-07-28 21:45:43 +0530

    Update image_color-to-grayscale_2.py

--- image_color-to-grayscale_2.py
+++ image_color-to-grayscale_2.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 import cv2
 import re
+import glob
 
 # pillow library used
 # OpenCV library used
@@ -13,69 +14,68 @@ print("\n TIF, JPG, PNG: Color to Grayscale mode Conversion \n")
 
 filepath = input("Enter the Input Image file path: ")
 output_directory = "Grayscale"
-output = filepath + "/" + output_directory
+output = filepath + "/" + output_directory + "/"
 
 if os.path.exists(output):
     pass
 else:
     os.mkdir(output)
 
-for fname in os.listdir(filepath):
-	if fname.endswith(".tif"):
-		print(fname)
+for fname in glob.glob(filepath + "/" + "*.tif"):
+	input_image = fname
+	image = cv2.imread(input_image)
+	filename = os.path.basename(fname)
+	print(filename)
 
-		input_image = filepath + "/" + fname
-		image = cv2.imread(input_image)
-		output_name = output + "/" + fname
+	# get the Image DPI value from the Input images
+	image1 = Image.open(input_image)
+	img_dpi = str(image1.info['dpi'])
+	patn = re.sub(r"[\(\)]", "", img_dpi)
+	sp = patn.split(",")[0]
+	dpi_val = round(float(sp))
 
-		# get the Image DPI value from the Input images
-		image1 = Image.open(input_image)
-		img_dpi = str(image1.info['dpi'])
-		patn = re.sub(r"[\(\)]", "", img_dpi)
-		sp = patn.split(",")[0]
-		dpi_val = round(float(sp))
+	# convert the Color image to Grayscale format
+	image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	save_image = Image.fromarray(image_conv)
+	save_image.save(output + filename, dpi=(dpi_val,dpi_val))
 
-		# convert the Color image to Grayscale format
-		image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		save_image = Image.fromarray(image_conv)
-		save_image.save(output_name, dpi=(dpi_val,dpi_val))
 
-for fname in os.listdir(filepath):
-	if fname.endswith(".jpg"):
-		print(fname)
+for fname in glob.glob(filepath + "/" + "*.jpg"):
+	input_image = fname
+	image = cv2.imread(input_image)
+	filename = os.path.basename(fname)
+	print(filename)
 
-		input_image = filepath + "/" + fname
-		image = cv2.imread(input_image)
-		output_name = output + "/" + fname
+	# get the Image DPI value from the Input images
+	image1 = Image.open(input_image)
+	img_dpi = str(image1.info['dpi'])
+	patn = re.sub(r"[\(\)]", "", img_dpi)
+	sp = patn.split(",")[0]
+	dpi_val = round(float(sp))
 
-		# get the Image DPI value from the Input images
-		image1 = Image.open(input_image)
-		img_dpi = str(image1.info['dpi'])
-		patn = re.sub(r"[\(\)]", "", img_dpi)
-		sp = patn.split(",")[0]
-		dpi_val = round(float(sp))
+	# convert the Color image to Grayscale format
+	image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	save_image = Image.fromarray(image_conv)
+	save_image.save(output + filename, dpi=(dpi_val,dpi_val))
 
-		# convert the Color image to Grayscale format
-		image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		save_image = Image.fromarray(image_conv)
-		save_image.save(output_name, dpi=(dpi_val,dpi_val))
 
-for fname in os.listdir(filepath):
-	if fname.endswith(".png"):
-		print(fname)
 
-		input_image = filepath + "/" + fname
-		image = cv2.imread(input_image)
-		output_name = output + "/" + fname
+for fname in glob.glob(filepath + "/" + "*.png"):
+	input_image = fname
+	image = cv2.imread(input_image)
+	filename = os.path.basename(fname)
+	print(filename)
 
-		# get the Image DPI value from the Input images
-		image1 = Image.open(input_image)
-		img_dpi = str(image1.info['dpi'])
-		patn = re.sub(r"[\(\)]", "", img_dpi)
-		sp = patn.split(",")[0]
-		dpi_val = round(float(sp))
+	# get the Image DPI value from the Input images
+	image1 = Image.open(input_image)
+	img_dpi = str(image1.info['dpi'])
+	patn = re.sub(r"[\(\)]", "", img_dpi)
+	sp = patn.split(",")[0]
+	dpi_val = round(float(sp))
 
-		# convert the Color image to Grayscale format
-		image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		save_image = Image.fromarray(image_conv)
-		save_image.save(output_name, dpi=(dpi_val,dpi_val))
+	# convert the Color image to Grayscale format
+	image_conv = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+	save_image = Image.fromarray(image_conv)
+	save_image.save(output + filename, dpi=(dpi_val,dpi_val))
+
+print("\n\n *** Process Completed *** \n")
